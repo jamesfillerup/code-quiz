88aa6adeae5ba8c0cdc1cc1d0a6bdf3a42982b03
jamesfill <jamesp.fillerup@gmail.com>
2022-03-07 14:25:17 -0700

    next question working

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,132 +31,124 @@
 // 5.4 after name is put into score and they click save it will take them back to the start page and it will display high score.
 
 // 6 start page shows up with high score
-// 6.1 after they press enter on high score page return to start page with high scores visible 
-var startButton = document.getElementById("start-btn")
-var nextQuestion = document.getElementsByClassName("next")
-var answerButtonsEl = document.getElementById("answer-buttons")
+// 6.1 after they press enter on high score page return to start page with high scores visible
+var startButton = document.getElementById("start-btn");
+var nextQuestion = document.getElementsByClassName("next");
+var answerButtonsEl = document.getElementById("answer-buttons");
 var questionContainerEl = document.getElementById("question-container");
-var questionEl = document.getElementById("question")
+var questionEl = document.getElementById("question");
 var currentQuestion = 0;
 var element = document.getElementById("timer");
 
 var score = 0;
-var timer = 5;
+var timer = 10;
 
 var questions = [
-    {
-        question: "What does CSS do?",
-        answers: [
-            { text: "Nothing", correct: false },
-            { text: "Adds style to HTML", correct: true },
-            { text: "It is the bones of the web page", correct: false },
-            { text: "Adds function to the web page", correct: false }
-        ]
-    },
-    {
-        question: 'How do you write and array in JavaScript?',
-        answers: [
-            { text: "var letters = ('a','b','c')", correct: true },
-            { text: "var letters = 'a','b','c'", correct: false },
-            { text: "var letters = (a,b,c)", correct: false },
-            { text: "var letters = a,b,c", correct: false }
-        ]
-    },
-    {
-        question: 'What is a way to write a function in Javascript?',
-        answers: [
-            { text: 'function;nameOfFunction()', correct: false },
-            { text: 'function nameOfFunction()', correct: true },
-            { text: 'function:nameOfFunction()', correct: false },
-            { text: 'function+nameOfFunction()', correct: false }
-        ]
-    },
-    {
-        question: 'How do you put Javascript into HTML?',
-        answers: [
-            { text: '<javascript>', correct: false },
-            { text: '<script>', correct: true },
-            { text: '<java>', correct: false },
-            { text: '<js>', correct: false }
-        ]
-    }
-]
+  {
+    question: "What does CSS do?",
+    answers: [
+      { text: "Nothing", correct: false },
+      { text: "Adds style to HTML", correct: true },
+      { text: "It is the bones of the web page", correct: false },
+      { text: "Adds function to the web page", correct: false },
+    ],
+  },
+  {
+    question: "How do you write and array in JavaScript?",
+    answers: [
+      { text: "var letters = ('a','b','c')", correct: true },
+      { text: "var letters = 'a','b','c'", correct: false },
+      { text: "var letters = (a,b,c)", correct: false },
+      { text: "var letters = a,b,c", correct: false },
+    ],
+  },
+  {
+    question: "What is a way to write a function in Javascript?",
+    answers: [
+      { text: "function;nameOfFunction()", correct: false },
+      { text: "function nameOfFunction()", correct: true },
+      { text: "function:nameOfFunction()", correct: false },
+      { text: "function+nameOfFunction()", correct: false },
+    ],
+  },
+  {
+    question: "How do you put Javascript into HTML?",
+    answers: [
+      //prettier-ignore
+      { text: "<javascript>", correct: false },
+      { text: "<script>", correct: true },
+      { text: "<java>", correct: false },
+      { text: "<js>", correct: false },
+    ],
+  },
+];
 
-// for (var i = 0 ; i < nextQuestion.length; i++) {
-//     nextQuestion[i].addEventListener('click', showNextQuestion(currentQuestion), false); 
-// }
-
-startButton.addEventListener('click', startGame)
-
-for (var i = 0 ; i < nextQuestion.length; i++) {
-    nextQuestion[i].setAttribute("onclick", "showNextQuestion();");
-    
+for (var i = 0; i < nextQuestion.length; i++) {
+  nextQuestion[i].addEventListener("click", showNextQuestion, false);
 }
 
+startButton.addEventListener("click", startGame);
+
+// for (var i = 0; i < nextQuestion.length; i++) {
+//   nextQuestion[i].setAttribute("onclick", "showNextQuestion();");
+// }
+
 function startGame() {
-    console.log("game is starting");
-    myTimer();
-    startButton.classList.add('hide')
-    questionContainerEl.classList.remove('hide')
-    // answerButtonsEl.classList.remove('hide')
-    currentQuestion = 0;
-    questions.length
-    showNextQuestion()}
+  console.log("game is starting");
+  myTimer();
+  startButton.classList.add("hide");
+  questionContainerEl.classList.remove("hide");
+  // answerButtonsEl.classList.remove('hide')
+  currentQuestion = 0;
+  showNextQuestion();
+}
 
 function showQuestion(question) {
-    questionEl.innerText = question.question
-    
+  questionEl.innerText = question.question;
 }
 
 function selectAnswer(e) {
-    console.log(e)
-    currentQuestion++;
-    var selectedButton = e.target
-    var correct = selectedButton.dataset.correct
-    // setStatusClass(document.body, correct)
-    console.log(correct);
-    if (!correct) {
-        timer = timer - 10;
-    } else if (correct) {
-        score += 5
-        document.querySelector("#score").innerHTML = score
-    }
+  console.log(e);
+  currentQuestion++;
+  var selectedButton = e.target;
+  var correct = selectedButton.dataset.correct;
+  // setStatusClass(document.body, correct)
+  console.log(correct);
+  if (!correct) {
+    timer = timer - 10;
+  } else if (correct) {
+    score += 5;
+    document.querySelector("#score").innerHTML = score;
+  }
 }
 
+function showNextQuestion() {
+  console.log("next question function");
+  // for(i=0; i<=questions.length; i++){
+  document.getElementById("question").innerHTML =
+    questions[currentQuestion].question;
+  for (let [key, value] of Object.entries(answerButtonsEl.children)) {
+    value.textContent = questions[currentQuestion].answers[key].text;
+  }
 
-function showNextQuestion(){
-    console.log("next question funciton");
-    // for(i=0; i<=questions.length; i++){
-        document.getElementById("question").innerHTML = questions[currentQuestion].question;
-        question.answers.forEach(answer => {
-            const button = document.createElement('button')
-            button.innerText = answer.text
-            button.classList.add('btn')
-            if (answer.correct) {
-                button.dataset.correct = answer.correct
-            }
-            button.addEventListener('click', selectAnswer)
-            answerButtonsElement.appendChild(button)
-        })
-
-        //i would add a if statment if(i=questions.length){display score card}
-    // }
+  //i would add a if statment if(i=questions.length){display score card}
+  // }
+  currentQuestion++;
 }
 
 document.getElementById("timer");
 
-function myTimer(){
-
-        myInterval = setInterval(function () {
-            console.log(timer);
-            timer--;
-            if (timer > 0) {
-                element.innerHTML = timer
-                
-            }
-            else {
-                clearInterval(myInterval)
-                element.innerHTML = ("You're out of time!")
-            }
-        }, 1000)
+function myTimer() {
+  myInterval = setInterval(function () {
+    console.log(timer);
+    timer--;
+    if (timer > 0) {
+      element.innerHTML = timer;
+    } else {
+      clearInterval(myInterval);
+      element.innerHTML = "You're out of time!";
+    }
+  }, 1000);
 }
+
+// Grab the question div and populate it
